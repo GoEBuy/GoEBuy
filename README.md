162cbc518f3ca2312e90bb9ed587bb28d76fff89
energy1010 <energy1010@github.com>
2018-07-15 12:28:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+#GoEBuyService
+
+
+=================
+maven repository:    http://mvnrepository.com/
+
+##技术介绍
+后端:
+hibernate
+jpa
+spring
+jetty/mysql
+
+前端：
+js
+bootstrap
+
+
+Better use the newest version of IntelliJ IDEA/eclipse, JDK 1.8_x, and Tomcat 8.x.
+
+---
---
+++ src/main/java/com/goebuy/service/BlogService.java
@@ -0,0 +1,24 @@
+package com.goebuy.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goebuy.entity.BlogEntity;
+
+import java.util.Date;
+
+@Repository
+public interface BlogService extends JpaRepository<BlogEntity, Integer> {
+
+    // 修改博文操作
+    @Modifying
+    @Transactional
+    @Query("update BlogEntity blog set blog.title=:qTitle, blog.userByUserId.id=:qUserId," +
+            " blog.content=:qContent, blog.pubDate=:qPubDate where blog.id=:qId")
+    void updateBlog(@Param("qTitle") String title, @Param("qUserId") int userId, @Param("qContent") String content,
+                    @Param("qPubDate") Date pubDate, @Param("qId") int id);
+}
