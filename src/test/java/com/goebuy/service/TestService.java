b4f8f3433249c76ea66fa370c1f411d06d5bb4a2
energy1010 <energy1010@github.com>
2018-07-17 20:13:35 +0800

    update

--- src/test/java/com/goebuy/service/TestService.java
+++ src/test/java/com/goebuy/service/TestService.java
@@ -1,16 +1,19 @@
-//package com.goebuy.service;
-//
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//import org.springframework.context.support.FileSystemXmlApplicationContext;
-//
-//public class TestService {
-//
-//	public static void main(String[] args) {
-////		 ApplicationContext applicationContext =new ClassPathXmlApplicationContext("classpath:mvc-dispatcher-servlet.xml");
-////		 用文件系统的路径,默认指项目的根路径
-//		ApplicationContext applicationContext = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml");
-//		 System.out.println("finished");
-//	}
-//
-//}
+package com.goebuy.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.goebuy.controller.MainController;
+
+public class TestService {
+
+	public static void main(String[] args) {
+//		 ApplicationContext applicationContext =new ClassPathXmlApplicationContext("classpath:mvc-dispatcher-servlet.xml");
+//		 用文件系统的路径,默认指项目的根路径
+		ApplicationContext applicationContext = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml");
+		MainController service = (MainController)applicationContext.getBean("MainController"); 
+		System.out.println("finished");
+	}
+
+}
