38445dd471d12f1946c13c869461c2d22e0b26ae
Knuke Yang <yyyang@tju.edu.cn>
2018-08-14 16:14:59 +0800

    add listener

---
+++ src/main/java/com/goebuy/listener/InitApplicationListener.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 惠购 - 惠购网 - www.huiget.com - 特别会购！
+ * Copyright © 2014 惠购 www.huiget.com 版权所有
+ */
+package com.goebuy.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.springframework.web.context.ContextLoader;
+
+//import com.huiget.mall.common.util.ApplicationContextUtil;
+
+public class InitApplicationListener extends ContextLoader implements ServletContextListener {
+
+    public InitApplicationListener() {
+    }
+
+    /** 
+     * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+     */
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        // 初始化Spring容器
+        ApplicationContextUtil.applicationContext = initWebApplicationContext(sce.getServletContext());
+    }
+
+    /** 
+     * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
+     */
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+        closeWebApplicationContext(sce.getServletContext());
+
+    }
+
+}
---
+++ src/test/java/com/goebuy/service/EmailTest.java
@@ -0,0 +1,13 @@
+package com.goebuy.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringRunner.class)
+@ContextConfiguration({"classpath:mvc-dispatcher-servlet.xml"})
+public class EmailTest {
+
+}
