4d5a70933e6a01d9e722da7267f8037446901f95
Knuke Yang <yyyang@tju.edu.cn>
2018-08-22 15:14:30 +0800

    添加群组表注释

--- src/main/java/com/goebuy/entity/user/UserGroup.java
+++ src/main/java/com/goebuy/entity/user/UserGroup.java
@@ -12,6 +12,11 @@ import javax.persistence.Table;
 
 /**
  * 用户群组表
+ * 包含: <ul>
+ * <li>用户事件群组表</li>
+ * <li>用户标签群组表</li>
+ * <li>用户自定义群组表</li>
+ * </ul>
  *
  * Created by luodejin on 2018/8/14.
  */
